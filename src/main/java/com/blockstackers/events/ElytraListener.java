e9bdc83300e8ae549f6aac94fe051317d9d97554
SafeKlez <harethpy@gmail.com>
2022-01-04 12:51:29 +0800

    Fixed Repeating task

--- src/main/java/com/blockstackers/events/ElytraListener.java
+++ src/main/java/com/blockstackers/events/ElytraListener.java
@@ -8,14 +8,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+import org.jetbrains.annotations.NotNull;
 
-public class ElytraListener implements Listener{
+public class ElytraListener implements Listener {
 
     elytranolag plugin;
     ConfigManager configManager;
 
-    public ElytraListener(elytranolag plugin)
-    {
+    public ElytraListener(elytranolag plugin) {
 
         this.plugin = plugin;
         this.configManager = new ConfigManager(plugin);
@@ -24,35 +27,34 @@ public class ElytraListener implements Listener{
     CooldownManager cooldown = new CooldownManager();
 
     @EventHandler
-    public void onFireworkGlide(PlayerElytraBoostEvent e)
-    {
+    public void onFireworkGlide(PlayerElytraBoostEvent e) {
 
         Player p = e.getPlayer();
 
-        if(configManager.existPlayer(p.getName()))
+        if (configManager.existPlayer(p.getName()))
             return;
 
 
         // Cancel Event & Set Cooldown
-        if(cooldown.checkExist(p.getUniqueId()) && cooldown.timeLeft(p.getUniqueId()) > 0)
-        {
+        if (cooldown.checkExist(p.getUniqueId()) && cooldown.timeLeft(p.getUniqueId()) > 0) {
             e.setCancelled(true);
         } else {
             cooldown.add(p.getUniqueId());
         }
 
         // Updates Cooldown message
-        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
+
+
+        BukkitTask task = new BukkitRunnable() {
             @Override
             public void run() {
 
-                if(cooldown.timeLeft(p.getUniqueId()) > 0)
-                {
+                if (cooldown.timeLeft(p.getUniqueId()) > 0) {
                     cooldown.displayCooldown(p);
+                } else {
+                    this.cancel();
                 }
             }
-        }, 0L, 20L);
-
-        }
+        }.runTaskTimer(plugin, 0L, 20L);
     }
-
+}
